949c035eca72ec8c559da9a77f9a872813bc65ba
birendra20 <birendragupta20@gmail.com>
2022-03-30 13:09:28 +0530

    login page update

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -8,6 +10,16 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Login />} />
+          <Route
+            exact
+            path="/home"
+            element={
+              <>
+                <Home /> <Header />{" "}
+              </>
+            }
+          />
+          {/* <Route exact path="/home" element={<Header />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
